d43409ae1ecd757b292d1201e9742c8726d3604f
tranbinhminh240807 <105159759+tranbinhminh240807@users.noreply.github.com>
2022-07-03 21:37:48 +0700

    Add files via upload

---
+++ bt.js
@@ -0,0 +1,22 @@
+// function student(name,age,address) {
+    
+// }
+// console.log( student)
+
+
+
+// var smartPhones = [ 
+//     { name: 'iphone', price: 649 }, 
+//     { name: 'Galaxy S6', price: 576 }, 
+//     { name: 'Galaxy Note 5', price: 489 } 
+// ];
+
+// // ES6 
+// console.log(smartPhones.map(smartPhone => smartPhone.price)); // [649, 576, 489]
+
+// const foo = (x,y,z ) => { return x,y,z };
+({a,b,m} = {m: a+b*100 } )=> m
+
+
+
+
